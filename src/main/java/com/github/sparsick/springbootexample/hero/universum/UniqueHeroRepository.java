367952b8cbb4ed807d3312bb0ed66ca52e43098d
sparsick <sparsick@web.de>
2020-03-06 17:43:14 +0100

    improve show case "which feature is used"

--- src/main/java/com/github/sparsick/springbootexample/hero/universum/UniqueHeroRepository.java
+++ src/main/java/com/github/sparsick/springbootexample/hero/universum/UniqueHeroRepository.java
@@ -1,23 +1,18 @@
 package com.github.sparsick.springbootexample.hero.universum;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import javax.annotation.PostConstruct;
-
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.stereotype.Repository;
+import java.util.stream.Collectors;
 
 @Repository
 public class UniqueHeroRepository implements HeroRepository {
 
     private Set<Hero> heroes = new HashSet<>();
-    private Counter addCounter;
-
-    public UniqueHeroRepository(MeterRegistry meterRegistry) {
-        addCounter = meterRegistry.counter("hero.repository.unique");
-    }
 
     @PostConstruct
     public void init() {
@@ -32,13 +27,17 @@ public class UniqueHeroRepository implements HeroRepository {
 
     @Override
     public void addHero(Hero hero) {
-        addCounter.increment();
         heroes.add(hero);
     }
 
     @Override
     public Collection<Hero> allHeros() {
         return new HashSet<>(heroes);
+    }
+
+    @Override
+    public Collection<Hero> findHerosBySearchCriteria(String searchCriteria) {
+        return heroes.stream().filter( hero -> StringUtils.containsIgnoreCase(hero.toString(), searchCriteria)).collect(Collectors.toSet());
     }
 
 }
